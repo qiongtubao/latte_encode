617e622a7fb0740468d687b17c09490f7dc90d25
qiongtubao <810776886@qq.com>
2018-04-04 18:01:01 +0800

    change test demo

--- test/utf8.js
+++ test/utf8.js
@@ -1,19 +1,19 @@
 var Utf8 = require('../index').utf8;
 describe('object', function() {
     it("decode", function(done) {
-        console.log(Utf8.decode("latteçä¸ç")) ; //latte的世界
+        console.log(Utf8.decode("ç¼ç ")) ; //latte的世界
         done();
     });
     it("encode", function(done) {
-        console.log(Utf8.encode("latte的世界")) ; //latte的世界
+        console.log(Utf8.encode("编码")) ; //latte的世界
         done();
     });
     it("ucs2decode", function(done) {
-        console.log(Utf8.ucs2decode("latte的世界")) ; //latte的世界
+        console.log(Utf8.ucs2decode("编码")) ; //latte的世界
         done();
     });
     it("ucs2encode", function(done) {
-        console.log(Utf8.ucs2encode([108,97,116,116,101,30340,19990,30028])) ; //latte的世界
+        console.log(Utf8.ucs2encode([ 32534, 30721 ])) ; //latte的世界
         done();
     });
 });
